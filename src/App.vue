017c3dd9e12241a18fd0db79137cc1cb080df6a5
zsh2517 <zsh@zsh2517.com>
2021-07-02 13:17:30 +0800

    三个页面基本结构

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <el-row :gutter="0">
+            <el-col :span="2">
+                <left-menu style="height: 100vh;"></left-menu>
+
+            </el-col>
+            <el-col :span="22" style="overflow-y: scroll; height:100vh;">
+                <div class="fullscreen-area" id="duty">
+                    <h1>排班管理</h1>
+                </div>
+                <hr>
+                <div class="fullscreen-area" id="process">
+                    <h1>进程管理</h1>
+                </div>
+                <hr>
+                <div class="fullscreen-area" id="course">
+                    <h1>课表管理</h1>
+                </div>
+            </el-col>
+        </el-row>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import leftMenu from './components/leftMenu'
+import duty from './components/duty';
+import process from './components/process';
+import course from './components/course';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        leftMenu,
+        duty,
+        process,
+        course
+    }
 }
 </script>
 
 <style>
+body {
+    margin: 0;
+}
+
+.fullscreen-area {
+    width: 100%;
+    height: 100vh;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+
 }
 </style>
---
+++ src/components/leftMenu.vue
@@ -0,0 +1,27 @@
+<template>
+    <el-menu>
+        <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <span slot="title"><a href="#duty">排班管理</a></span>
+        </el-menu-item>
+        <el-menu-item index="3">
+            <i class="el-icon-document"></i>
+            <span slot="title"><a href="#process">进程管理</a></span>
+
+        </el-menu-item>
+        <el-menu-item index="4">
+            <i class="el-icon-setting"></i>
+            <span slot="title"><a href="#course">课表管理</a></span>
+        </el-menu-item>
+    </el-menu>
+</template>
+
+<script>
+export default {
+    name: "leftMenu"
+}
+</script>
+
+<style scoped>
+
+</style>
